2238841b606dec0be7ff216a5d48bf522251babd
Spencer McIntyre <zeroSteiner@gmail.com>
2015-03-20 10:41:16 -0400

    Add a burp python plugin template

---
+++ geany/templates/files/python_burp.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+{fileheader}
+
+from burp import IBurpExtender
+from burp import IHttpListener
+
+class BurpExtender(IBurpExtender):
+	def __init__(self, *args, **kwargs):
+		super(BurpExtender, self).__init__(*args, **kwargs)
+
+	def registerExtenderCallbacks(self, callbacks):
+		self.callbacks = callbacks
+		self.callbacks.registerHttpListener(HttpListener())
+
+class HttpListener(IHttpListener):
+	def __init__(self, *args, **kwargs):
+		super(HttpListener, self).__init__(*args, **kwargs)
+
+	def processHttpMessage(self, tool_ame, is_request, message_info):
+		if not is_request:
+			return
